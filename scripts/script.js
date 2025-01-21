d6d00449d093343b738cfb465d90c8a325a14e03
Thevnaka De Silva <thevnakadesilva@Thevnakas-MacBook-Air.local>
2025-01-21 10:28:06 +0530

    Import

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/GAME FORGE/scripts/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker Registered:', registration);
-      })
-      .catch((error) => {
-        console.log('Service Worker Registration Failed:', error);
-      });
-  }
-  
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/GAME FORGE/scripts/service-worker.js')
+//       .then((registration) => {
+//         console.log('Service Worker Registered:', registration);
+//       })
+//       .catch((error) => {
+//         console.log('Service Worker Registration Failed:', error);
+//       });
+//   }
+
+
